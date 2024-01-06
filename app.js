62bcfd35f1120e206d59e52359443c34439416b3
chieuma <chieub2005665@student.ctu.edu.vn>
2024-01-06 15:39:26 +0700

    apierror app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express =  require('express');
 const  contactRouter = require('./app/routes/contact.route');
 
 const cors = require('cors');
+const ApiError = require('./app/api-error');
  
 const app = express();
 
@@ -14,5 +15,15 @@ app.get("/", (req, res) =>{
 });
 
 app.use("/api/contacts", contactRouter);
-module.exports = app;
 
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",   
+    });
+});
+
+module.exports = app;
